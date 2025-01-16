977d12e70bf7240c512be2dc0c7de12cf94708c2
Subroto Banerjee <subroto.2003@gmail.com>
2025-01-16 11:58:23 +0530

    Create assignment1.sh

---
+++ assignment1.sh
@@ -0,0 +1,20 @@
+# ques 2
+$hostname
+
+# ques 3
+$ping -c 10 google.com
+
+# ques 4
+$netstat
+
+# ques 5
+$arp -a
+
+# ques 6
+$ifconfig eno1 down
+$ifconfig eno1 up
+
+#ques 7
+$nslookup google.com
+
+#ques 8
